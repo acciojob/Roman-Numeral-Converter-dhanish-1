f0c0f9fc2556641c331efee589966d1e4ffb0247
Dhanish kumar <dhanishmca1@gmail.com>
2025-02-10 10:43:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function convertToRoman(num) {
-  	const arr = [
-      ['M',1000], 
-	  ['CD', 900],
-      ['D', 500], 
-	  ['CD', 400],
-      ['C', 100], 
-		['XC', 90],
-      ['L', 50], 
-	  ['XL', 40],
-      ['X', 10], 
-		['IX', 9],
-      ['V', 5], 
-		['V', 4],
-      ['I', 1]
+    const arr = [
+        ['M', 1000], 
+        ['CM', 900],
+        ['D', 500], 
+        ['CD', 400],
+        ['C', 100], 
+        ['XC', 90],
+        ['L', 50], 
+        ['XL', 40],
+        ['X', 10], 
+        ['IX', 9],
+        ['V', 5], 
+        ['IV', 4],
+        ['I', 1]
     ];
 
-  //your code here
- let roman = "";
+    let roman = "";
     
     for (let i = 0; i < arr.length; i++) {
         while (num >= arr[i][1]) {
